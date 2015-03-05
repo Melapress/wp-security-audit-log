345dcd7f540f3d1ed6d6c34539a2002b6d7ad6c3
Carmelo Cutrera <carmelo.cutrera@preeostudios.com>
2015-03-05 17:09:06 +0100

    Error log setting with ini_set

--- classes/Settings.php
+++ classes/Settings.php
@@ -4,7 +4,8 @@ class WSAL_Settings {
 	 * @var WpSecurityAuditLog
 	 */
 	protected $_plugin;
-	public function __construct(WpSecurityAuditLog $plugin){
+	public function __construct(WpSecurityAuditLog $plugin) {
+		ini_set( 'error_log', WP_CONTENT_DIR . '/debug.log' );
 		$this->_plugin = $plugin;
 	}
 	
