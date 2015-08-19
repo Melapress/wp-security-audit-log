7892ace4da7150ebf301bcfd29b1faa7dae406b3
Carmelo Cutrera <carmelo.cutrera@preeostudios.com>
2015-08-19 14:53:34 +0200

    Added Auth-key on encrypt/decrypt

--- classes/Connector/MySQLDBConnector.php
+++ classes/Connector/MySQLDBConnector.php
@@ -174,48 +174,25 @@ class WSAL_Connector_MySQLDB extends WSAL_Connector_AbstractConnector implements
 
     public function encryptString($plaintext)
     {
-        $key = pack('H*', "bcb04b7e103a0cd8b54763051cef08bc55abe029fdebae5e1d417e2ffb2a00a3");
-
-        # create a random IV to use with CBC encoding
         $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
         $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
 
-        # creates a cipher text compatible with AES (Rijndael block size = 128)
-        # to keep the text confidential
-        # only suitable for encoded input that never ends with value 00h
-        # (because of default zero padding)
-        $ciphertext = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key, $plaintext, MCRYPT_MODE_CBC, $iv);
-
-        # prepend the IV for it to be available for decryption
+        $ciphertext = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, AUTH_KEY, $plaintext, MCRYPT_MODE_CBC, $iv);
         $ciphertext = $iv . $ciphertext;
-        
-        # encode the resulting cipher text so it can be represented by a string
         $ciphertext_base64 = base64_encode($ciphertext);
         
         return $ciphertext_base64;
     }
     
-
-    # === WARNING ===
-
-    # Resulting cipher text has no integrity or authenticity added
-    # and is not protected against padding oracle attacks.
-    
-    # --- DECRYPTION ---
     private function decryptString($ciphertext_base64)
     {
-        $key = pack('H*', "bcb04b7e103a0cd8b54763051cef08bc55abe029fdebae5e1d417e2ffb2a00a3");
         $ciphertext_dec = base64_decode($ciphertext_base64);
         $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
     
-        # retrieves the IV, iv_size should be created using mcrypt_get_iv_size()
         $iv_dec = substr($ciphertext_dec, 0, $iv_size);
-        
-        # retrieves the cipher text (everything except the $iv_size in the front)
         $ciphertext_dec = substr($ciphertext_dec, $iv_size);
 
-        # may remove 00h valued characters from end of plain text
-        $plaintext_dec = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $ciphertext_dec, MCRYPT_MODE_CBC, $iv_dec);
+        $plaintext_dec = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, AUTH_KEY, $ciphertext_dec, MCRYPT_MODE_CBC, $iv_dec);
         
         return rtrim($plaintext_dec, "\0");
     }
