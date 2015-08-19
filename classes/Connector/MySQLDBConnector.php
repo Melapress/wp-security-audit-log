70c6d4eaf958b2afce30ad5af6a7f01a40648472
Carmelo Cutrera <carmelo.cutrera@preeostudios.com>
2015-08-19 13:49:07 +0200

    Encrypt/decrypt password DB

--- classes/Connector/MySQLDBConnector.php
+++ classes/Connector/MySQLDBConnector.php
@@ -6,11 +6,13 @@ require_once('AbstractConnector.php');
 class WSAL_Connector_MySQLDB extends WSAL_Connector_AbstractConnector implements WSAL_Connector_ConnectorInterface
 {
     protected $connectionConfig = null;
+    protected $key = null;
     public function __construct($connectionConfig = null)
     {
         $this->connectionConfig = $connectionConfig;
         parent::__construct("MySQL");
         require_once($this->getAdaptersDirectory() . '/OptionAdapter.php');
+        $this->key = pack('H*', "bcb04b7e103a0cd8b54763051cef08bc55abe029fdebae5e1d417e2ffb2a00a3");
     }
 
     function test_wp_die_callback() {
@@ -37,7 +39,8 @@ class WSAL_Connector_MySQLDB extends WSAL_Connector_AbstractConnector implements
         if (!empty($this->connectionConfig)) {
             //TO DO: Use the provided connection config
             $connectionConfig = $this->connectionConfig;
-            $newWpdb = new wpdb($connectionConfig['user'], $connectionConfig['password'], $connectionConfig['name'], $connectionConfig['hostname']);
+            $password = $this->decryptString($connectionConfig['password']);
+            $newWpdb = new wpdb($connectionConfig['user'], $password, $connectionConfig['name'], $connectionConfig['hostname']);
             $newWpdb->set_prefix($connectionConfig['base_prefix']);
             return $newWpdb;
         } else {
@@ -101,7 +104,7 @@ class WSAL_Connector_MySQLDB extends WSAL_Connector_AbstractConnector implements
         $plugin = WpSecurityAuditLog::GetInstance();
 
         foreach (glob($this->getAdaptersDirectory() . DIRECTORY_SEPARATOR . '*.php') as $file) {
-            $filePath = explode(DIRECTORY_SEPARATOR, $file); 
+            $filePath = explode(DIRECTORY_SEPARATOR, $file);
             $fileName = $filePath[count($filePath) - 1];
             $className = $this->getAdapterClassName(str_replace("Adapter.php", "", $fileName));
 
@@ -147,9 +150,9 @@ class WSAL_Connector_MySQLDB extends WSAL_Connector_AbstractConnector implements
 
         // Insert data
         $occurrenceNew = new WSAL_Adapters_MySQL_Occurrence($_wpdb);
-        $sql = 'INSERT INTO ' . $occurrenceNew->GetTable() . ' (id, site_id, alert_id, created_on, is_read, is_migrated) VALUES ' ;
+        $sql = 'INSERT INTO ' . $occurrenceNew->GetTable() . ' (site_id, alert_id, created_on, is_read, is_migrated) VALUES ' ;
         foreach ($occurrences as $entry) {
-            $sql .= '('.$entry['id'].', '.$entry['site_id'].', '.$entry['alert_id'].', '.$entry['created_on'].', '.$entry['is_read'].', 1), ';
+            $sql .= '('.$entry['site_id'].', '.$entry['alert_id'].', '.$entry['created_on'].', '.$entry['is_read'].', 1), ';
         }
         $sql = rtrim($sql, ", ");
         $_wpdb->query($sql);
@@ -161,12 +164,60 @@ class WSAL_Connector_MySQLDB extends WSAL_Connector_AbstractConnector implements
 
         // Insert data
         $metaNew = new WSAL_Adapters_MySQL_Meta($_wpdb);
-        $sql = 'INSERT INTO ' . $metaNew->GetTable() . ' (id, occurrence_id, name, value) VALUES ' ;
+        $sql = 'INSERT INTO ' . $metaNew->GetTable() . ' (occurrence_id, name, value) VALUES ' ;
         foreach ($metadata as $entry) {
-            $sql .= '('.$entry['id'].', '.$entry['occurrence_id'].', \''.$entry['name'].'\', \''.$entry['value'].'\'), ';
+            $sql .= '('.$entry['occurrence_id'].', \''.$entry['name'].'\', \''.$entry['value'].'\'), ';
         }
         $sql = rtrim($sql, ", ");
         $_wpdb->query($sql);
+    }
+
+    public function encryptString($plaintext)
+    {
+        $key = pack('H*', "bcb04b7e103a0cd8b54763051cef08bc55abe029fdebae5e1d417e2ffb2a00a3");
+
+        # create a random IV to use with CBC encoding
+        $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+        $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+
+        # creates a cipher text compatible with AES (Rijndael block size = 128)
+        # to keep the text confidential
+        # only suitable for encoded input that never ends with value 00h
+        # (because of default zero padding)
+        $ciphertext = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key, $plaintext, MCRYPT_MODE_CBC, $iv);
+
+        # prepend the IV for it to be available for decryption
+        $ciphertext = $iv . $ciphertext;
+        
+        # encode the resulting cipher text so it can be represented by a string
+        $ciphertext_base64 = base64_encode($ciphertext);
+        
+        return $ciphertext_base64;
+    }
+    
+
+    # === WARNING ===
+
+    # Resulting cipher text has no integrity or authenticity added
+    # and is not protected against padding oracle attacks.
+    
+    # --- DECRYPTION ---
+    private function decryptString($ciphertext_base64)
+    {
+        $key = pack('H*', "bcb04b7e103a0cd8b54763051cef08bc55abe029fdebae5e1d417e2ffb2a00a3");
+        $ciphertext_dec = base64_decode($ciphertext_base64);
+        $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+    
+        # retrieves the IV, iv_size should be created using mcrypt_get_iv_size()
+        $iv_dec = substr($ciphertext_dec, 0, $iv_size);
+        
+        # retrieves the cipher text (everything except the $iv_size in the front)
+        $ciphertext_dec = substr($ciphertext_dec, $iv_size);
+
+        # may remove 00h valued characters from end of plain text
+        $plaintext_dec = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $ciphertext_dec, MCRYPT_MODE_CBC, $iv_dec);
+        
+        return rtrim($plaintext_dec, "\0");
     }
     
 }
