d1641d741c07ec470affdd06953f0ab27d264a1d
Carmelo Cutrera <carmelo.cutrera@preeostudios.com>
2015-08-25 10:44:02 +0200

    Truncate key for encript password

--- classes/Connector/MySQLDBConnector.php
+++ classes/Connector/MySQLDBConnector.php
@@ -175,8 +175,8 @@ class WSAL_Connector_MySQLDB extends WSAL_Connector_AbstractConnector implements
     {
         $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
         $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
-
-        $ciphertext = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, AUTH_KEY, $plaintext, MCRYPT_MODE_CBC, $iv);
+        $key = $this->truncateKey();
+        $ciphertext = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key, $plaintext, MCRYPT_MODE_CBC, $iv);
         $ciphertext = $iv . $ciphertext;
         $ciphertext_base64 = base64_encode($ciphertext);
         
@@ -190,9 +190,19 @@ class WSAL_Connector_MySQLDB extends WSAL_Connector_AbstractConnector implements
     
         $iv_dec = substr($ciphertext_dec, 0, $iv_size);
         $ciphertext_dec = substr($ciphertext_dec, $iv_size);
-
-        $plaintext_dec = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, AUTH_KEY, $ciphertext_dec, MCRYPT_MODE_CBC, $iv_dec);
+        $key = $this->truncateKey();
+        $plaintext_dec = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $ciphertext_dec, MCRYPT_MODE_CBC, $iv_dec);
         
         return rtrim($plaintext_dec, "\0");
+    }
+
+    private function truncateKey()
+    {
+        $key_size =  strlen(AUTH_KEY);
+        if ($key_size > 32) {
+            return substr(AUTH_KEY, 0, 32);
+        } else {
+            return AUTH_KEY;
+        }
     }
 }
