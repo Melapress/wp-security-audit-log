c36dffb021904d54d75e8e26fa889830d90c3fb4
Robert Abela <robert@wpwhitesecurity.com>
2015-11-05 07:04:42 +0000

    deleted extra file

--- Request.log.php
+++
@@ -1,3 +0,0 @@
-<?php die('Access Denied'); ?>
-[2015-08-03 12:59:43] POST /wp-admin/admin-ajax.php 
-                      {"interval":"60","_nonce":"7ce01a7fb0","action":"heartbeat","screen_id":"plugins-network","has_focus":"false"}
