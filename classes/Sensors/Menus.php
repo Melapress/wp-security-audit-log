64c3827193ccacc06441358d10b7b0450b6b9b07
Carmelo Cutrera <carmelo.cutrera@preeostudios.com>
2016-02-19 13:34:37 +0100

    Added hooks for events (2079, 2080, 2082) on customizer

--- classes/Sensors/Menus.php
+++ classes/Sensors/Menus.php
@@ -4,7 +4,7 @@ class WSAL_Sensors_Menus extends WSAL_AbstractSensor
 {
     protected $_OldMenu = null;
     protected $_OldMenuTerms = array();
-    protected $_OldMenuItems = null;
+    protected $_OldMenuItems = array();
     protected $_OldMenuLocations = null;
 
     public function HookEvents()
@@ -13,7 +13,7 @@ class WSAL_Sensors_Menus extends WSAL_AbstractSensor
         add_action('wp_delete_nav_menu', array($this, 'DeleteMenu'), 10, 1);
         add_action('wp_update_nav_menu', array($this, 'UpdateMenu'), 10, 2);
         add_action('admin_init', array($this, 'EventAdminInit'));
-        // Customizer
+        // Customizer trigger
         add_action('customize_register', array($this, 'CustomizeInit'));
         add_action('customize_save_after', array($this, 'CustomizeSave'));
     }
@@ -59,25 +59,16 @@ class WSAL_Sensors_Menus extends WSAL_AbstractSensor
                     if (count($addedNames) > 0 && count($addedTypes) > 0) {
                         $contentName = implode(",", $addedNames);
                         $contentType = implode(",", array_unique($addedTypes));
-
-                        $this->plugin->alerts->Trigger(2079, array(
-                            'ContentType' => $contentType,
-                            'ContentName' => $contentName,
-                            'MenuName' => $menu_data['menu-name']
-                        ));
+                        $this->EventAddItems($contentType, $contentName, $menu_data['menu-name']);
                     }
+
                     $removedNames = array_diff_assoc($contentNamesOld, $contentNamesNew);
                     $removedTypes = array_diff_assoc($contentTypesOld, $contentTypesNew);
                     // Remove items from the menu
                     if (count($removedNames) > 0 && count($removedTypes) > 0) {
                         $contentName = implode(",", $removedNames);
                         $contentType = implode(",", array_unique($removedTypes));
-
-                        $this->plugin->alerts->Trigger(2080, array(
-                            'ContentType' => $contentType,
-                            'ContentName' => $contentName,
-                            'MenuName' => $menu_data['menu-name']
-                        ));
+                        $this->EventRemoveItems($contentType, $contentName, $menu_data['menu-name']);
                     }
 
                     // Enable/Disable menu setting
@@ -141,7 +132,7 @@ class WSAL_Sensors_Menus extends WSAL_AbstractSensor
                     }
                     // Alert 2082 Social links menu
                     if (!empty($locationSocial)) {
-                        $this->EventMenuSetting($menu_data['menu-name'], $locationSocial, "Location: social links menu");
+                        $this->EventMenuSetting($menu_data['menu-name'], $locationSocial, "Location: social menu");
                     }
                 }
             }
@@ -167,8 +158,15 @@ class WSAL_Sensors_Menus extends WSAL_AbstractSensor
         if (!empty($menus)) {
             foreach ($menus as $menu) {
                 array_push($this->_OldMenuTerms, $menu->name);
+                $items = wp_get_nav_menu_items($menu->term_id);
+                if (!empty($items)) {
+                    foreach ($items as $item) {
+                        array_push($this->_OldMenuItems, array('item_id' => $item->ID, 'title' => $item->title, 'object' => $item->object, 'menu_name' => $menu->name));
+                    }
+                }
             }
         }
+        $this->_OldMenuLocations = get_nav_menu_locations();
     }
 
     public function CustomizeSave()
@@ -190,6 +188,72 @@ class WSAL_Sensors_Menus extends WSAL_AbstractSensor
                 }
             }
         }
+        if (isset($_POST['action']) && $_POST['action'] == 'customize_save') {
+            if (isset($_POST['wp_customize'], $_POST['customized'])) {
+                $customized = json_decode(wp_unslash($_POST['customized']), true);
+                if (is_array($customized)) {
+                    foreach ($customized as $key => $value) {
+                        if (!empty($value['nav_menu_term_id'])) {
+                            // Add Items to the menu
+                            $menu = wp_get_nav_menu_object($value['nav_menu_term_id']);
+                            $content_name = !empty($value['title']) ? $value['title'] : "no title";
+                            $this->EventAddItems($value['type_label'], $content_name, $menu->name);
+                        } else {
+                            // Setting Auto add pages
+                            if (!empty($value) && isset($value['auto_add'])) {
+                                if ($value['auto_add']) {
+                                    $this->EventMenuSetting($value['name'], 'Enabled', "Auto add pages");
+                                } else {
+                                    $this->EventMenuSetting($value['name'], 'Disabled', "Auto add pages");
+                                }
+                            }
+                            // Setting Location
+                            if (false !== strpos($key, 'nav_menu_locations[')) {
+                                $loc = substr(trim($key, ']'), 19);
+                                if (!empty($value)) {
+                                    $menu = wp_get_nav_menu_object($value);
+                                    $this->EventMenuSetting($menu->name, "Enabled", "Location: ".$loc." menu");
+                                } else {
+                                    if (!empty($this->_OldMenuLocations[$loc])) {
+                                        $menu = wp_get_nav_menu_object($this->_OldMenuLocations[$loc]);
+                                        $this->EventMenuSetting($menu->name, "Disabled", "Location: ".$loc." menu");
+                                    }
+                                }
+                            }
+                            // Remove items from the menu
+                            if (false !== strpos($key, 'nav_menu_item[')) {
+                                $item_id = substr(trim($key, ']'), 14);
+                                if (!empty($this->_OldMenuItems)) {
+                                    foreach ($this->_OldMenuItems as $old_item) {
+                                        if ($old_item['item_id'] == $item_id) {
+                                            $this->EventRemoveItems($old_item['object'], $old_item['title'], $old_item['menu_name']);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private function EventAddItems($content_type, $content_name, $menu_name)
+    {
+        $this->plugin->alerts->Trigger(2079, array(
+            'ContentType' => $content_type,
+            'ContentName' => $content_name,
+            'MenuName' => $menu_name
+        ));
+    }
+
+    private function EventRemoveItems($content_type, $content_name, $menu_name)
+    {
+        $this->plugin->alerts->Trigger(2080, array(
+            'ContentType' => $content_type,
+            'ContentName' => $content_name,
+            'MenuName' => $menu_name
+        ));
     }
 
     private function EventMenuSetting($menu_name, $status, $menu_setting)
