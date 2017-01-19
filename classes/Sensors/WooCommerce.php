fc9cf8742e31170236f412bf6a5e919557331a05
Carmelo Cutrera <carmelo.cutrera@preeostudios.com>
2017-01-19 12:28:35 +0100

    Added alert till 9007

--- classes/Sensors/WooCommerce.php
+++ classes/Sensors/WooCommerce.php
@@ -6,6 +6,8 @@ class WSAL_Sensors_WooCommerce extends WSAL_AbstractSensor
 {
     protected $_OldPost = null;
     protected $_OldLink = null;
+    protected $_OldCats = null;
+    protected $_OldData = null;
 
     public function HookEvents()
     {
@@ -19,6 +21,13 @@ class WSAL_Sensors_WooCommerce extends WSAL_AbstractSensor
 
         add_action('create_product_cat', array($this, 'EventCategoryCreation'), 10, 1);
         // add_action('edit_product_cat', array($this, 'EventCategoryChanged'), 10, 1);
+        do_action('get_the_terms', array($this, 'EventTest'), 10, 2);
+    }
+
+    public function EventTest($first, $second = null)
+    {
+        error_log(print_r($first, true));
+        error_log(print_r($second, true));
     }
 
     public function EventAdminInit()
@@ -36,6 +45,8 @@ class WSAL_Sensors_WooCommerce extends WSAL_AbstractSensor
             $postID = intval($_POST['post_ID']);
             $this->_OldPost = get_post($postID);
             $this->_OldLink = get_permalink($postID);
+            $this->_OldCats = $this->GetProductCategories($this->_OldPost);
+            $this->_OldData = $this->GetProductData($this->_OldPost);
         }
     }
 
@@ -43,23 +54,171 @@ class WSAL_Sensors_WooCommerce extends WSAL_AbstractSensor
     {
         if ($this->CheckWooCommerce($oldpost)) {
             $changes = 0 + $this->EventCreation($oldpost, $newpost);
-            // Change Visibility
             if (!$changes) {
-                //$changes = $this->EventChangedVisibility($oldpost);
+                // Change Categories
+                $changes = $this->CheckCategoriesChange($this->_OldCats, $this->GetProductCategories($newpost), $newpost);
             }
-            // Change Type
             if (!$changes) {
-                //$changes = $this->EventChangedType($oldpost);
+                // Change short description AND text
+                $changes = 0
+                    + $this->CheckShortDescriptionChange($oldpost, $newpost)
+                    + $this->CheckTextChange($oldpost, $newpost)
+                    + $this->CheckPermalinkChange($this->_OldLink, get_permalink($post_ID), $newpost);
+                    + $this->CheckProductDataChange($this->_OldData, $newpost);
+                ;
             }
-            // Change status
             if (!$changes) {
+                // Change status
                 //$changes = $this->EventChangedStatus($oldpost);
             }
-            // Change Order, Parent or URL
             if (!$changes) {
+                // Change Order, Parent or URL
                 //$changes = $this->EventChanged($oldpost, $newpost);
             }
         }
+    }
+
+    /**
+     * Trigger events 9000, 9001
+     */
+    private function EventCreation($old_post, $new_post)
+    {
+        $original = isset($_POST['original_post_status']) ? $_POST['original_post_status'] : '';
+        if ($old_post->post_status == 'draft' || $original == 'auto-draft') {
+            if ($old_post->post_type == 'product') {
+                $editorLink = $this->GetEditorLink($new_post);
+                if ($new_post->post_status == 'draft') {
+                    $this->plugin->alerts->Trigger(9000, array(
+                        'ProductTitle' => $new_post->post_title,
+                        $editorLink['name'] => $editorLink['value']
+                    ));
+                    return 1;
+                } else if ($new_post->post_status == 'publish') {
+                    $this->plugin->alerts->Trigger(9001, array(
+                        'ProductTitle' => $new_post->post_title,
+                        'ProductUrl' => get_permalink($new_post->ID),
+                        $editorLink['name'] => $editorLink['value']
+                    ));
+                    return 1;
+                }
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * Trigger events 9002
+     */
+    public function EventCategoryCreation($term_id = null)
+    {
+        $term = get_term($term_id);
+        if (!empty($term)) {
+            $this->plugin->alerts->Trigger(9002, array(
+                'CategoryName' => $term->name,
+                'Slug' => $term->slug
+            ));
+        }
+    }
+
+    /**
+     * Not implemented
+     */
+    public function EventCategoryChanged($term_id = null)
+    {
+        $old_term = get_term($term_id);
+        if (isset($_POST['taxonomy'])) {
+            // new $term in $_POST
+        }
+    }
+
+    /**
+     * Trigger events 9003
+     */
+    protected function CheckCategoriesChange($oldCats, $newCats, $post)
+    {
+        $oldCats = implode(', ', $oldCats);
+        $newCats = implode(', ', $newCats);
+        if ($oldCats != $newCats) {
+            $editorLink = $this->GetEditorLink($post);
+            $this->plugin->alerts->Trigger(9003, array(
+                'ProductTitle' => $post->post_title,
+                'OldCategories' => $oldCats ? $oldCats : 'no categories',
+                'NewCategories' => $newCats ? $newCats : 'no categories',
+                $editorLink['name'] => $editorLink['value']
+            ));
+            return 1;
+        }
+        return 0;
+    }
+
+    /**
+     * Trigger events 9004
+     */
+    protected function CheckShortDescriptionChange($oldpost, $newpost)
+    {
+        if ($oldpost->post_excerpt != $newpost->post_excerpt) {
+            $editorLink = $this->GetEditorLink($oldpost);
+            $this->plugin->alerts->Trigger(9004, array(
+                'ProductTitle' => $oldpost->post_title,
+                $editorLink['name'] => $editorLink['value']
+            ));
+            return 1;
+        }
+        return 0;
+    }
+
+    /**
+     * Trigger events 9005
+     */
+    protected function CheckTextChange($oldpost, $newpost)
+    {
+        if ($oldpost->post_content != $newpost->post_content) {
+            $editorLink = $this->GetEditorLink($oldpost);
+            $this->plugin->alerts->Trigger(9005, array(
+                'ProductTitle' => $oldpost->post_title,
+                $editorLink['name'] => $editorLink['value']
+            ));
+            return 1;
+        }
+        return 0;
+    }
+
+    /**
+     * Trigger events 9006
+     */
+    protected function CheckPermalinkChange($oldLink, $newLink, $post)
+    {
+        if ($oldLink != $newLink) {
+            $editorLink = $this->GetEditorLink($post);
+            $this->plugin->alerts->Trigger(9006, array(
+                'ProductTitle' => $post->post_title,
+                'OldUrl' => $oldLink,
+                'NewUrl' => $newLink,
+                $editorLink['name'] => $editorLink['value']
+            ));
+            return 1;
+        }
+        return 0;
+    }
+
+    /**
+     * Trigger events 9007
+     */
+    protected function CheckProductDataChange($oldData, $post)
+    {
+        if (isset($_POST['product-type'])) {
+            $oldData = implode(', ', $oldData);
+            $newData = $_POST['product-type'];
+            if ($oldData != $newData) {
+                $editorLink = $this->GetEditorLink($post);
+                $this->plugin->alerts->Trigger(9007, array(
+                    'ProductTitle' => $post->post_title,
+                    $editorLink['name'] => $editorLink['value']
+                ));
+                return 1;
+            }
+        }
+        return 0;
     }
 
     /**
@@ -95,51 +254,14 @@ class WSAL_Sensors_WooCommerce extends WSAL_AbstractSensor
         }
     }
 
-    private function EventCreation($old_post, $new_post)
+    protected function GetProductCategories($post)
     {
-        $original = isset($_POST['original_post_status']) ? $_POST['original_post_status'] : '';
-        if ($old_post->post_status == 'draft' || $original == 'auto-draft') {
-            if ($old_post->post_type == 'product') {
-                $editorLink = $this->GetEditorLink($new_post);
-                if ($new_post->post_status == 'draft') {
-                    $this->plugin->alerts->Trigger(9000, array(
-                        'ProductTitle' => $new_post->post_title,
-                        $editorLink['name'] => $editorLink['value']
-                    ));
-                    return 1;
-                } else if ($new_post->post_status == 'publish') {
-                    $this->plugin->alerts->Trigger(9001, array(
-                        'ProductTitle' => $new_post->post_title,
-                        'ProductUrl' => get_permalink($new_post->ID),
-                        $editorLink['name'] => $editorLink['value']
-                    ));
-                    return 1;
-                }
-            }
-        }
-        return 0;
+        return wp_get_post_terms($post->ID, 'product_cat', array("fields" => "names"));
     }
 
-    public function EventCategoryCreation($term_id = null)
+    protected function GetProductData($post)
     {
-        $term = get_term($term_id);
-        if (!empty($term)) {
-            $this->plugin->alerts->Trigger(9002, array(
-                'CategoryName' => $term->name,
-                'Slug' => $term->slug
-            ));
-        }
-    }
-
-    /**
-     * Not implemented
-     */
-    public function EventCategoryChanged($term_id = null)
-    {
-        $old_term = get_term($term_id);
-        if (isset($_POST['taxonomy'])) {
-            // new $term in $_POST
-        }
+        return wp_get_post_terms($post->ID, 'product_type', array("fields" => "names"));
     }
 
     private function CheckWooCommerce($post)
