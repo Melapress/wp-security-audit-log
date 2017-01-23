bd748d723276e96998b40433ed3b5e9771fbbe40
Carmelo Cutrera <carmelo.cutrera@preeostudios.com>
2017-01-23 12:59:14 +0100

    Added all the alerts of woocommerce sensor

--- classes/Sensors/WooCommerce.php
+++ classes/Sensors/WooCommerce.php
@@ -9,6 +9,8 @@ class WSAL_Sensors_WooCommerce extends WSAL_AbstractSensor
     protected $_OldCats = null;
     protected $_OldData = null;
     protected $_OldStockStatus = null;
+    protected $_OldFileNames = array();
+    protected $_OldFileUrls = array();
 
     public function HookEvents()
     {
@@ -28,6 +30,7 @@ class WSAL_Sensors_WooCommerce extends WSAL_AbstractSensor
     {
         // load old data, if applicable
         $this->RetrieveOldData();
+        $this->CheckSettingsChange();
     }
 
     protected function RetrieveOldData()
@@ -42,6 +45,14 @@ class WSAL_Sensors_WooCommerce extends WSAL_AbstractSensor
             $this->_OldCats = $this->GetProductCategories($this->_OldPost);
             $this->_OldData = $this->GetProductData($this->_OldPost);
             $this->_OldStockStatus = get_post_meta($postID, '_stock_status', true);
+
+            $oldDownloadableFiles  = get_post_meta($postID, '_downloadable_files', true);
+            if (!empty($oldDownloadableFiles)) {
+                foreach ($oldDownloadableFiles as $file) {
+                    array_push($this->_OldFileNames, $file['name']);
+                    array_push($this->_OldFileUrls, $file['file']);
+                }
+            }
         }
     }
 
@@ -78,12 +89,8 @@ class WSAL_Sensors_WooCommerce extends WSAL_AbstractSensor
                 }
             }
             if (!$changes) {
-                // Change status
+                // Change Permalink
                 $changes = $this->CheckPermalinkChange($this->_OldLink, get_post_permalink($post_ID), $newpost);
-            }
-            if (!$changes) {
-                // Change Order, Parent or URL
-                //$changes = $this->EventChanged($oldpost, $newpost);
             }
         }
     }
@@ -567,53 +574,159 @@ class WSAL_Sensors_WooCommerce extends WSAL_AbstractSensor
     }
 
     /**
-     * Trigger events 9023, 9024
+     * Trigger events 9023, 9024, 9025, 9026
      */
     protected function CheckDownloadableFileChange($oldpost)
     {
         $result = 0;
-        $oldFileNames = array();
-        $oldFileUrls = array();
-        $oldDownloadableFiles  = get_post_meta($oldpost->ID, '_downloadable_files', true);
-        if (!empty($oldDownloadableFiles)) {
-            foreach ($oldDownloadableFiles as $file) {
-                array_push($oldFileNames, $file['name']);
-                array_push($oldFileUrls, $file['file']);
-            }
-        }
+        $isUrlChanged = false;
+        $isNameChanged = false;
         $newFileNames = !empty($_POST['_wc_file_names']) ? $_POST['_wc_file_names'] : array();
         $newFileUrls = !empty($_POST['_wc_file_urls']) ? $_POST['_wc_file_urls'] : array();
+        $editorLink = $this->GetEditorLink($oldpost);
 
-        $addedUrls = array_diff($newFileUrls, $oldFileUrls);
+        $addedUrls = array_diff($newFileUrls, $this->_OldFileUrls);
         // Added files to the product
         if (count($addedUrls) > 0) {
-            $editorLink = $this->GetEditorLink($oldpost);
-            foreach ($addedUrls as $key => $url) {
-                $this->plugin->alerts->Trigger(9023, array(
-                    'ProductTitle' => $oldpost->post_title,
-                    'FileName' => $newFileNames[$key],
-                    'FileUrl' => $url,
-                    $editorLink['name'] => $editorLink['value']
-                ));
+            // if the file has only changed URL
+            if (count($newFileUrls) == count($this->_OldFileUrls)) {
+                $isUrlChanged = true;
+            } else {
+                foreach ($addedUrls as $key => $url) {
+                    $this->plugin->alerts->Trigger(9023, array(
+                        'ProductTitle' => $oldpost->post_title,
+                        'FileName' => $newFileNames[$key],
+                        'FileUrl' => $url,
+                        $editorLink['name'] => $editorLink['value']
+                    ));
+                }
+                $result = 1;
             }
-            $result = 1;
         }
 
-        $removedUrls = array_diff($oldFileUrls, $newFileUrls);
+        $removedUrls = array_diff($this->_OldFileUrls, $newFileUrls);
         // Removed files from the product
         if (count($removedUrls) > 0) {
-            $editorLink = $this->GetEditorLink($oldpost);
-            foreach ($removedUrls as $key => $url) {
-                $this->plugin->alerts->Trigger(9024, array(
+            // if the file has only changed URL
+            if (count($newFileUrls) == count($this->_OldFileUrls)) {
+                $isUrlChanged = true;
+            } else {
+                foreach ($removedUrls as $key => $url) {
+                    $this->plugin->alerts->Trigger(9024, array(
+                        'ProductTitle' => $oldpost->post_title,
+                        'FileName' => $this->_OldFileNames[$key],
+                        'FileUrl' => $url,
+                        $editorLink['name'] => $editorLink['value']
+                    ));
+                }
+                $result = 1;
+            }
+        }
+
+        $addedNames = array_diff($newFileNames, $this->_OldFileNames);
+        if (count($addedNames) > 0) {
+            // if the file has only changed Name
+            if (count($newFileNames) == count($this->_OldFileNames)) {
+                foreach ($addedNames as $key => $name) {
+                    $this->plugin->alerts->Trigger(9025, array(
+                        'ProductTitle' => $oldpost->post_title,
+                        'OldName' => $this->_OldFileNames[$key],
+                        'NewName' => $name,
+                        $editorLink['name'] => $editorLink['value']
+                    ));
+                }
+                $result = 1;
+            }
+        }
+
+        if ($isUrlChanged) {
+            foreach ($addedUrls as $key => $url) {
+                $this->plugin->alerts->Trigger(9026, array(
                     'ProductTitle' => $oldpost->post_title,
-                    'FileName' => $oldFileNames[$key],
-                    'FileUrl' => $url,
+                    'FileName' => $newFileNames[$key],
+                    'OldUrl' => $removedUrls[$key],
+                    'NewUrl' => $url,
                     $editorLink['name'] => $editorLink['value']
                 ));
             }
             $result = 1;
         }
         return $result;
+    }
+
+    /**
+     * Trigger events Settings: 9027, 9028, 9029, 9030, 9031, 9032, 9033
+     */
+    protected function CheckSettingsChange()
+    {
+        if (isset($_GET['page']) && $_GET['page'] == 'wc-settings') {
+            if (isset($_POST['woocommerce_weight_unit'])) {
+                $oldUnit = $this->GetConfig('weight_unit');
+                $newUnit = $_POST['woocommerce_weight_unit'];
+                if ($oldUnit != $newUnit) {
+                    $this->plugin->alerts->Trigger(9027, array(
+                        'OldUnit' => $oldUnit,
+                        'NewUnit' => $newUnit
+                    ));
+                }
+            }
+            if (isset($_POST['woocommerce_dimension_unit'])) {
+                $oldUnit = $this->GetConfig('dimension_unit');
+                $newUnit = $_POST['woocommerce_dimension_unit'];
+                if ($oldUnit != $newUnit) {
+                    $this->plugin->alerts->Trigger(9028, array(
+                        'OldUnit' => $oldUnit,
+                        'NewUnit' => $newUnit
+                    ));
+                }
+            }
+            if (isset($_POST['woocommerce_default_country'])) {
+                $oldLocation = $this->GetConfig('default_country');
+                $newLocation = $_POST['woocommerce_default_country'];
+                if ($oldLocation != $newLocation) {
+                    $this->plugin->alerts->Trigger(9029, array(
+                        'OldLocation' => $oldLocation,
+                        'NewLocation' => $newLocation
+                    ));
+                }
+                $oldCalcTaxes = $this->GetConfig('calc_taxes');
+                $newCalcTaxes = isset($_POST['woocommerce_calc_taxes']) ? 'yes' : 'no';
+                if ($oldCalcTaxes != $newCalcTaxes) {
+                    $status = ($newCalcTaxes == 'yes') ? 'Enabled' : 'Disabled';
+                    $this->plugin->alerts->Trigger(9030, array(
+                        'Status' => $status,
+                    ));
+                }
+            }
+            if (isset($_POST['woocommerce_currency'])) {
+                $oldCurrency = $this->GetConfig('currency');
+                $newCurrency = $_POST['woocommerce_currency'];
+                if ($oldCurrency != $newCurrency) {
+                    $this->plugin->alerts->Trigger(9031, array(
+                        'OldCurrency' => $oldCurrency,
+                        'NewCurrency' => $newCurrency
+                    ));
+                }
+            }
+            if (!empty($_POST)) {
+                $oldEnableCoupons = $this->GetConfig('enable_coupons');
+                $newEnableCoupons = isset($_POST['woocommerce_enable_coupons']) ? 'yes' : 'no';
+                if ($oldEnableCoupons != $newEnableCoupons) {
+                    $status = ($newEnableCoupons == 'yes') ? 'Enabled' : 'Disabled';
+                    $this->plugin->alerts->Trigger(9032, array(
+                        'Status' => $status,
+                    ));
+                }
+                $oldEnableGuestCheckout = $this->GetConfig('enable_guest_checkout');
+                $newEnableGuestCheckout = isset($_POST['woocommerce_enable_guest_checkout']) ? 'yes' : 'no';
+                if ($oldEnableGuestCheckout != $newEnableGuestCheckout) {
+                    $status = ($newEnableGuestCheckout == 'yes') ? 'Enabled' : 'Disabled';
+                    $this->plugin->alerts->Trigger(9033, array(
+                        'Status' => $status,
+                    ));
+                }
+            }
+        }
     }
 
     private function GetStockStatusName($slug)
@@ -674,97 +787,30 @@ class WSAL_Sensors_WooCommerce extends WSAL_AbstractSensor
     private function GetCurrencySymbol($currency = '')
     {
         $symbols = array(
-            'AED' => '&#x62f;.&#x625;', 'AFN' => '&#x60b;', 'ALL' => 'L', 'AMD' => 'AMD', 'ANG' => '&fnof;',
-            'AOA' => 'Kz', 'ARS' => '&#36;', 'AUD' => '&#36;', 'AWG' => '&fnof;', 'AZN' => 'AZN', 'BAM' => 'KM',
-            'BBD' => '&#36;', 'BDT' => '&#2547;&nbsp;', 'BGN' => '&#1083;&#1074;.', 'BHD' => '.&#x62f;.&#x628;',
-            'BIF' => 'Fr', 'BMD' => '&#36;', 'BND' => '&#36;', 'BOB' => 'Bs.', 'BRL' => '&#82;&#36;', 'BSD' => '&#36;',
-            'BTC' => '&#3647;', 'BTN' => 'Nu.', 'BWP' => 'P', 'BYR' => 'Br', 'BZD' => '&#36;', 'CAD' => '&#36;', 'CDF' => 'Fr',
-            'CHF' => '&#67;&#72;&#70;', 'CLP' => '&#36;', 'CNY' => '&yen;', 'COP' => '&#36;', 'CRC' => '&#x20a1;', 'CUC' => '&#36;',
-            'CUP' => '&#36;', 'CVE' => '&#36;', 'CZK' => '&#75;&#269;', 'DJF' => 'Fr', 'DKK' => 'DKK', 'DOP' => 'RD&#36;',
-            'DZD' => '&#x62f;.&#x62c;', 'EGP' => 'EGP', 'ERN' => 'Nfk', 'ETB' => 'Br', 'EUR' => '&euro;', 'FJD' => '&#36;',
-            'FKP' => '&pound;', 'GBP' => '&pound;', 'GEL' => '&#x10da;', 'GGP' => '&pound;', 'GHS' => '&#x20b5;', 'GIP' => '&pound;',
-            'GMD' => 'D', 'GNF' => 'Fr', 'GTQ' => 'Q', 'GYD' => '&#36;', 'HKD' => '&#36;', 'HNL' => 'L', 'HRK' => 'Kn',
-            'HTG' => 'G', 'HUF' => '&#70;&#116;', 'IDR' => 'Rp', 'ILS' => '&#8362;', 'IMP' => '&pound;','INR' => '&#8377;',
-            'IQD' => '&#x639;.&#x62f;', 'IRR' => '&#xfdfc;', 'ISK' => 'kr.', 'JEP' => '&pound;', 'JMD' => '&#36;',
-            'JOD' => '&#x62f;.&#x627;', 'JPY' => '&yen;', 'KES' => 'KSh', 'KGS' => '&#x441;&#x43e;&#x43c;', 'KHR' => '&#x17db;',
-            'KMF' => 'Fr', 'KPW' => '&#x20a9;', 'KRW' => '&#8361;', 'KWD' => '&#x62f;.&#x643;', 'KYD' => '&#36;', 'KZT' => 'KZT',
-            'LAK' => '&#8365;', 'LBP' => '&#x644;.&#x644;', 'LKR' => '&#xdbb;&#xdd4;', 'LRD' => '&#36;', 'LSL' => 'L',
-            'LYD' => '&#x644;.&#x62f;',
-            'MAD' => '&#x62f;. &#x645;.',
-            'MAD' => '&#x62f;.&#x645;.',
-            'MDL' => 'L',
-            'MGA' => 'Ar',
-            'MKD' => '&#x434;&#x435;&#x43d;',
-            'MMK' => 'Ks',
-            'MNT' => '&#x20ae;',
-            'MOP' => 'P',
-            'MRO' => 'UM',
-            'MUR' => '&#x20a8;',
-            'MVR' => '.&#x783;',
-            'MWK' => 'MK',
-            'MXN' => '&#36;',
-            'MYR' => '&#82;&#77;',
-            'MZN' => 'MT',
-            'NAD' => '&#36;',
-            'NGN' => '&#8358;',
-            'NIO' => 'C&#36;',
-            'NOK' => '&#107;&#114;',
-            'NPR' => '&#8360;',
-            'NZD' => '&#36;',
-            'OMR' => '&#x631;.&#x639;.',
-            'PAB' => 'B/.',
-            'PEN' => 'S/.',
-            'PGK' => 'K',
-            'PHP' => '&#8369;',
-            'PKR' => '&#8360;',
-            'PLN' => '&#122;&#322;',
-            'PRB' => '&#x440;.',
-            'PYG' => '&#8370;',
-            'QAR' => '&#x631;.&#x642;',
-            'RMB' => '&yen;',
-            'RON' => 'lei',
-            'RSD' => '&#x434;&#x438;&#x43d;.',
-            'RUB' => '&#8381;',
-            'RWF' => 'Fr',
-            'SAR' => '&#x631;.&#x633;',
-            'SBD' => '&#36;',
-            'SCR' => '&#x20a8;',
-            'SDG' => '&#x62c;.&#x633;.',
-            'SEK' => '&#107;&#114;',
-            'SGD' => '&#36;',
-            'SHP' => '&pound;',
-            'SLL' => 'Le',
-            'SOS' => 'Sh',
-            'SRD' => '&#36;',
-            'SSP' => '&pound;',
-            'STD' => 'Db',
-            'SYP' => '&#x644;.&#x633;',
-            'SZL' => 'L',
-            'THB' => '&#3647;',
-            'TJS' => '&#x405;&#x41c;',
-            'TMT' => 'm',
-            'TND' => '&#x62f;.&#x62a;',
-            'TOP' => 'T&#36;',
-            'TRY' => '&#8378;',
-            'TTD' => '&#36;',
-            'TWD' => '&#78;&#84;&#36;',
-            'TZS' => 'Sh',
-            'UAH' => '&#8372;',
-            'UGX' => 'UGX',
-            'USD' => '&#36;',
-            'UYU' => '&#36;',
-            'UZS' => 'UZS',
-            'VEF' => 'Bs F',
-            'VND' => '&#8363;',
-            'VUV' => 'Vt',
-            'WST' => 'T',
-            'XAF' => 'Fr',
-            'XCD' => '&#36;',
-            'XOF' => 'Fr',
-            'XPF' => 'Fr',
-            'YER' => '&#xfdfc;',
-            'ZAR' => '&#82;',
-            'ZMW' => 'ZK',
+            'AED' => '&#x62f;.&#x625;','AFN' => '&#x60b;','ALL' => 'L','AMD' => 'AMD','ANG' => '&fnof;','AOA' => 'Kz','ARS' => '&#36;',
+            'AUD' => '&#36;','AWG' => '&fnof;','AZN' => 'AZN','BAM' => 'KM','BBD' => '&#36;','BDT' => '&#2547;&nbsp;','BGN' => '&#1083;&#1074;.',
+            'BHD' => '.&#x62f;.&#x628;','BIF' => 'Fr','BMD' => '&#36;','BND' => '&#36;','BOB' => 'Bs.','BRL' => '&#82;&#36;','BSD' => '&#36;',
+            'BTC' => '&#3647;','BTN' => 'Nu.','BWP' => 'P','BYR' => 'Br','BZD' => '&#36;','CAD' => '&#36;','CDF' => 'Fr','CHF' => '&#67;&#72;&#70;',
+            'CLP' => '&#36;','CNY' => '&yen;','COP' => '&#36;','CRC' => '&#x20a1;','CUC' => '&#36;','CUP' => '&#36;','CVE' => '&#36;',
+            'CZK' => '&#75;&#269;','DJF' => 'Fr','DKK' => 'DKK','DOP' => 'RD&#36;','DZD' => '&#x62f;.&#x62c;','EGP' => 'EGP','ERN' => 'Nfk',
+            'ETB' => 'Br','EUR' => '&euro;','FJD' => '&#36;','FKP' => '&pound;','GBP' => '&pound;','GEL' => '&#x10da;','GGP' => '&pound;',
+            'GHS' => '&#x20b5;','GIP' => '&pound;','GMD' => 'D','GNF' => 'Fr','GTQ' => 'Q','GYD' => '&#36;','HKD' => '&#36;','HNL' => 'L',
+            'HRK' => 'Kn','HTG' => 'G','HUF' => '&#70;&#116;','IDR' => 'Rp','ILS' => '&#8362;','IMP' => '&pound;','INR' => '&#8377;',
+            'IQD' => '&#x639;.&#x62f;','IRR' => '&#xfdfc;','ISK' => 'kr.','JEP' => '&pound;','JMD' => '&#36;','JOD' => '&#x62f;.&#x627;',
+            'JPY' => '&yen;','KES' => 'KSh','KGS' => '&#x441;&#x43e;&#x43c;','KHR' => '&#x17db;','KMF' => 'Fr','KPW' => '&#x20a9;','KRW' => '&#8361;',
+            'KWD' => '&#x62f;.&#x643;','KYD' => '&#36;','KZT' => 'KZT','LAK' => '&#8365;','LBP' => '&#x644;.&#x644;','LKR' => '&#xdbb;&#xdd4;',
+            'LRD' => '&#36;','LSL' => 'L','LYD' => '&#x644;.&#x62f;','MAD' => '&#x62f;. &#x645;.','MAD' => '&#x62f;.&#x645;.','MDL' => 'L','MGA' => 'Ar',
+            'MKD' => '&#x434;&#x435;&#x43d;','MMK' => 'Ks','MNT' => '&#x20ae;','MOP' => 'P','MRO' => 'UM','MUR' => '&#x20a8;','MVR' => '.&#x783;',
+            'MWK' => 'MK','MXN' => '&#36;','MYR' => '&#82;&#77;','MZN' => 'MT','NAD' => '&#36;','NGN' => '&#8358;','NIO' => 'C&#36;',
+            'NOK' => '&#107;&#114;','NPR' => '&#8360;','NZD' => '&#36;','OMR' => '&#x631;.&#x639;.','PAB' => 'B/.','PEN' => 'S/.',
+            'PGK' => 'K','PHP' => '&#8369;','PKR' => '&#8360;','PLN' => '&#122;&#322;','PRB' => '&#x440;.','PYG' => '&#8370;','QAR' => '&#x631;.&#x642;',
+            'RMB' => '&yen;','RON' => 'lei','RSD' => '&#x434;&#x438;&#x43d;.','RUB' => '&#8381;','RWF' => 'Fr','SAR' => '&#x631;.&#x633;',
+            'SBD' => '&#36;','SCR' => '&#x20a8;','SDG' => '&#x62c;.&#x633;.','SEK' => '&#107;&#114;','SGD' => '&#36;','SHP' => '&pound;','SLL' => 'Le',
+            'SOS' => 'Sh','SRD' => '&#36;','SSP' => '&pound;','STD' => 'Db','SYP' => '&#x644;.&#x633;','SZL' => 'L','THB' => '&#3647;',
+            'TJS' => '&#x405;&#x41c;','TMT' => 'm','TND' => '&#x62f;.&#x62a;','TOP' => 'T&#36;','TRY' => '&#8378;','TTD' => '&#36;',
+            'TWD' => '&#78;&#84;&#36;','TZS' => 'Sh','UAH' => '&#8372;','UGX' => 'UGX','USD' => '&#36;','UYU' => '&#36;','UZS' => 'UZS',
+            'VEF' => 'Bs F','VND' => '&#8363;','VUV' => 'Vt','WST' => 'T','XAF' => 'Fr','XCD' => '&#36;','XOF' => 'Fr','XPF' => 'Fr',
+            'YER' => '&#xfdfc;','ZAR' => '&#82;','ZMW' => 'ZK',
         );
         $currency_symbol = isset($symbols[$currency]) ? $symbols[ $currency ] : '';
 
