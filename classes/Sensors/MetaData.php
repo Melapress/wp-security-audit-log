eb18a8a42178df3685539523e7d1c579588d3c9f
Ashar Irfan <mrasharirfan@gmail.com>
2017-07-28 21:21:52 +0500

    Post meta data create and update alert issues are fixed 💯

--- classes/Sensors/MetaData.php
+++ classes/Sensors/MetaData.php
@@ -24,14 +24,13 @@ class WSAL_Sensors_MetaData extends WSAL_AbstractSensor
     /**
      * Listening to events using WP hooks.
      */
-    public function HookEvents()
-    {
-        add_action('add_post_meta', array($this,   'EventPostMetaCreated'), 10, 3);
-        add_action('update_post_meta', array($this,  'EventPostMetaUpdating'), 10, 3);
-        add_action('updated_post_meta', array($this, 'EventPostMetaUpdated'), 10, 4);
-        add_action('deleted_post_meta', array($this, 'EventPostMetaDeleted'), 10, 4);
+    public function HookEvents() {
+        add_action( 'add_post_meta', array( $this, 'EventPostMetaCreated' ), 10, 3 );
+        add_action( 'update_post_meta', array( $this, 'EventPostMetaUpdating' ), 10, 3 );
+        add_action( 'updated_post_meta', array( $this, 'EventPostMetaUpdated' ), 10, 4 );
+        add_action( 'deleted_post_meta', array( $this, 'EventPostMetaDeleted' ), 10, 4 );
     }
-    
+
     /**
      * Check "Excluded Custom Fields" or meta keys starts with "_".
      * @return boolean can log true|false
@@ -84,52 +83,54 @@ class WSAL_Sensors_MetaData extends WSAL_AbstractSensor
     /**
      * Created a custom field.
      */
-    public function EventPostMetaCreated($object_id, $meta_key, $meta_value)
-    {
-        $post = get_post($object_id);
-        if (!$this->CanLogPostMeta($object_id, $meta_key)) {
+    public function EventPostMetaCreated( $object_id, $meta_key, $meta_value ) {
+        $post = get_post( $object_id );
+        if ( ! $this->CanLogPostMeta( $object_id, $meta_key ) ) {
             return;
         }
 
-        $WPActions = array('add-meta');
-        if (isset($_POST['action']) && in_array($_POST['action'], $WPActions)) {
-            $editorLink = $this->GetEditorLink($post);
-            switch ($post->post_type) {
+        if ( 'revision' == $post->post_type ) {
+            return;
+        }
+
+        if ( isset( $_POST['action'] ) && 'editpost' == $_POST['action'] ) {
+            $editorLink = $this->GetEditorLink( $post );
+            switch ( $post->post_type ) {
                 case 'page':
-                    $this->plugin->alerts->Trigger(2059, array(
+                    $this->plugin->alerts->Trigger( 2059, array(
                         'PostID' => $object_id,
                         'PostTitle' => $post->post_title,
                         'MetaKey' => $meta_key,
                         'MetaValue' => $meta_value,
                         'MetaLink' => $meta_key,
-                        $editorLink['name'] => $editorLink['value']
-                    ));
+                        $editorLink['name'] => $editorLink['value'],
+                    ) );
                     break;
                 case 'post':
-                    $this->plugin->alerts->Trigger(2053, array(
+                    $this->plugin->alerts->Trigger( 2053, array(
                         'PostID' => $object_id,
                         'PostTitle' => $post->post_title,
                         'MetaKey' => $meta_key,
                         'MetaValue' => $meta_value,
                         'MetaLink' => $meta_key,
-                        $editorLink['name'] => $editorLink['value']
-                    ));
+                        $editorLink['name'] => $editorLink['value'],
+                    ) );
                     break;
                 default:
-                    $this->plugin->alerts->Trigger(2056, array(
+                    $this->plugin->alerts->Trigger( 2056, array(
                         'PostID' => $object_id,
                         'PostTitle' => $post->post_title,
                         'PostType' => $post->post_type,
                         'MetaKey' => $meta_key,
                         'MetaValue' => $meta_value,
                         'MetaLink' => $meta_key,
-                        $editorLink['name'] => $editorLink['value']
-                    ));
+                        $editorLink['name'] => $editorLink['value'],
+                    ) );
                     break;
             }
         }
     }
-    
+
     /**
      * Sets the old meta.
      */
@@ -141,26 +142,28 @@ class WSAL_Sensors_MetaData extends WSAL_AbstractSensor
             'val' => get_metadata($meta_type, $object_id, $meta_key, true),
         );
     }
-    
+
     /**
      * Updated a custom field name/value.
      */
-    public function EventPostMetaUpdated($meta_id, $object_id, $meta_key, $meta_value)
-    {
-        $post = get_post($object_id);
-        if (!$this->CanLogPostMeta($object_id, $meta_key)) {
+    public function EventPostMetaUpdated( $meta_id, $object_id, $meta_key, $meta_value ) {
+        $post = get_post( $object_id );
+        if ( ! $this->CanLogPostMeta( $object_id, $meta_key ) ) {
             return;
         }
 
-        $WPActions = array('add-meta');
-        if (isset($_POST['action']) && in_array($_POST['action'], $WPActions)) {
-            $editorLink = $this->GetEditorLink($post);
-            if (isset($this->old_meta[$meta_id])) {
-                // check change in meta key
-                if ($this->old_meta[$meta_id]->key != $meta_key) {
-                    switch ($post->post_type) {
+        if ( 'revision' == $post->post_type ) {
+            return;
+        }
+
+        if ( isset( $_POST['action'] ) && 'editpost' == $_POST['action'] ) {
+            $editorLink = $this->GetEditorLink( $post );
+            if ( isset( $this->old_meta[ $meta_id ] ) ) {
+                // Check change in meta key.
+                if ( $this->old_meta[ $meta_id ]->key != $meta_key ) {
+                    switch ( $post->post_type ) {
                         case 'page':
-                            $this->plugin->alerts->Trigger(2064, array(
+                            $this->plugin->alerts->Trigger( 2064, array(
                                 'PostID' => $object_id,
                                 'PostTitle' => $post->post_title,
                                 'MetaID' => $meta_id,
@@ -168,11 +171,11 @@ class WSAL_Sensors_MetaData extends WSAL_AbstractSensor
                                 'MetaKeyOld' => $this->old_meta[$meta_id]->key,
                                 'MetaValue' => $meta_value,
                                 'MetaLink' => $meta_key,
-                                $editorLink['name'] => $editorLink['value']
-                            ));
+                                $editorLink['name'] => $editorLink['value'],
+                            ) );
                             break;
                         case 'post':
-                            $this->plugin->alerts->Trigger(2062, array(
+                            $this->plugin->alerts->Trigger( 2062, array(
                                 'PostID' => $object_id,
                                 'PostTitle' => $post->post_title,
                                 'MetaID' => $meta_id,
@@ -180,11 +183,11 @@ class WSAL_Sensors_MetaData extends WSAL_AbstractSensor
                                 'MetaKeyOld' => $this->old_meta[$meta_id]->key,
                                 'MetaValue' => $meta_value,
                                 'MetaLink' => $meta_key,
-                                $editorLink['name'] => $editorLink['value']
-                            ));
+                                $editorLink['name'] => $editorLink['value'],
+                            ) );
                             break;
                         default:
-                            $this->plugin->alerts->Trigger(2063, array(
+                            $this->plugin->alerts->Trigger( 2063, array(
                                 'PostID' => $object_id,
                                 'PostTitle' => $post->post_title,
                                 'PostType' => $post->post_type,
@@ -193,14 +196,14 @@ class WSAL_Sensors_MetaData extends WSAL_AbstractSensor
                                 'MetaKeyOld' => $this->old_meta[$meta_id]->key,
                                 'MetaValue' => $meta_value,
                                 'MetaLink' => $smeta_key,
-                                $editorLink['name'] => $editorLink['value']
-                            ));
+                                $editorLink['name'] => $editorLink['value'],
+                            ) );
                             break;
                     }
-                } else if ($this->old_meta[$meta_id]->val != $meta_value) { // check change in meta value
-                    switch ($post->post_type) {
+                } elseif ( $this->old_meta[ $meta_id ]->val != $meta_value ) { // Check change in meta value.
+                    switch ( $post->post_type ) {
                         case 'page':
-                            $this->plugin->alerts->Trigger(2060, array(
+                            $this->plugin->alerts->Trigger( 2060, array(
                                 'PostID' => $object_id,
                                 'PostTitle' => $post->post_title,
                                 'MetaID' => $meta_id,
@@ -208,11 +211,11 @@ class WSAL_Sensors_MetaData extends WSAL_AbstractSensor
                                 'MetaValueNew' => $meta_value,
                                 'MetaValueOld' => $this->old_meta[$meta_id]->val,
                                 'MetaLink' => $meta_key,
-                                $editorLink['name'] => $editorLink['value']
-                            ));
+                                $editorLink['name'] => $editorLink['value'],
+                            ) );
                             break;
                         case 'post':
-                            $this->plugin->alerts->Trigger(2054, array(
+                            $this->plugin->alerts->Trigger( 2054, array(
                                 'PostID' => $object_id,
                                 'PostTitle' => $post->post_title,
                                 'MetaID' => $meta_id,
@@ -220,11 +223,11 @@ class WSAL_Sensors_MetaData extends WSAL_AbstractSensor
                                 'MetaValueNew' => $meta_value,
                                 'MetaValueOld' => $this->old_meta[$meta_id]->val,
                                 'MetaLink' => $meta_key,
-                                $editorLink['name'] => $editorLink['value']
-                            ));
+                                $editorLink['name'] => $editorLink['value'],
+                            ) );
                             break;
                         default:
-                            $this->plugin->alerts->Trigger(2057, array(
+                            $this->plugin->alerts->Trigger( 2057, array(
                                 'PostID' => $object_id,
                                 'PostTitle' => $post->post_title,
                                 'PostType' => $post->post_type,
@@ -233,13 +236,13 @@ class WSAL_Sensors_MetaData extends WSAL_AbstractSensor
                                 'MetaValueNew' => $meta_value,
                                 'MetaValueOld' => $this->old_meta[$meta_id]->val,
                                 'MetaLink' => $meta_key,
-                                $editorLink['name'] => $editorLink['value']
-                            ));
+                                $editorLink['name'] => $editorLink['value'],
+                            ) );
                             break;
                     }
                 }
-                // remove old meta update data
-                unset($this->old_meta[$meta_id]);
+                // Remove old meta update data.
+                unset( $this->old_meta[ $meta_id ] );
             }
         }
     }
@@ -247,10 +250,9 @@ class WSAL_Sensors_MetaData extends WSAL_AbstractSensor
     /**
      * Deleted a custom field.
      */
-    public function EventPostMetaDeleted($meta_ids, $object_id, $meta_key, $meta_value)
-    {
+    public function EventPostMetaDeleted($meta_ids, $object_id, $meta_key, $meta_value) {
         $post = get_post($object_id);
-        
+
         $WPActions = array('delete-meta');
         if (isset($_POST['action']) && in_array($_POST['action'], $WPActions)) {
             $editorLink = $this->GetEditorLink($post);
