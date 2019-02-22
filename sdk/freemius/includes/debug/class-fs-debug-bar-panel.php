1c85e108d6642ea42b932af2540281f2ebab058e
Ashar Irfan <mrasharirfan@gmail.com>
2019-02-22 10:15:45 +0500

    👌 IMPROVE: Freemius Debug Bar Panel Class

--- sdk/freemius/includes/debug/class-fs-debug-bar-panel.php
+++ sdk/freemius/includes/debug/class-fs-debug-bar-panel.php
@@ -6,9 +6,9 @@
 	 * @since       1.1.7.3
 	 */
 
-	if ( ! defined( 'ABSPATH' ) ) {
-		exit;
-	}
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 	/**
 	 * Extends Debug Bar plugin by adding a panel to show all Freemius API requests.
@@ -18,47 +18,47 @@
 	 *
 	 * Class Freemius_Debug_Bar_Panel
 	 */
-	class Freemius_Debug_Bar_Panel extends Debug_Bar_Panel {
-		function init() {
-			$this->title( 'Freemius' );
+class Freemius_Debug_Bar_Panel extends Debug_Bar_Panel {
+	public function init() {
+		$this->title( 'Freemius' );
+	}
+
+	public static function requests_count() {
+		if ( class_exists( 'Freemius_Api_WordPress' ) ) {
+			$logger = Freemius_Api_WordPress::GetLogger();
+		} else {
+			$logger = array();
 		}
 
-		static function requests_count() {
-			if ( class_exists( 'Freemius_Api_WordPress' ) ) {
-				$logger = Freemius_Api_WordPress::GetLogger();
-			} else {
-				$logger = array();
-			}
+		return number_format( count( $logger ) );
+	}
 
-			return number_format( count( $logger ) );
+	public static function total_time() {
+		if ( class_exists( 'Freemius_Api_WordPress' ) ) {
+			$logger = Freemius_Api_WordPress::GetLogger();
+		} else {
+			$logger = array();
 		}
 
-		static function total_time() {
-			if ( class_exists( 'Freemius_Api_WordPress' ) ) {
-				$logger = Freemius_Api_WordPress::GetLogger();
-			} else {
-				$logger = array();
-			}
-
-			$total_time = .0;
-			foreach ( $logger as $l ) {
-				$total_time += $l['total'];
-			}
-
-			return number_format( 100 * $total_time, 2 ) . ' ' . fs_text_x_inline( 'ms', 'milliseconds' );
+		$total_time = .0;
+		foreach ( $logger as $l ) {
+			$total_time += $l['total'];
 		}
 
-		function render() {
-			?>
-			<div id='debug-bar-php'>
-				<?php fs_require_template( '/debug/api-calls.php' ) ?>
-				<br>
-				<?php fs_require_template( '/debug/scheduled-crons.php' ) ?>
-				<br>
-				<?php fs_require_template( '/debug/plugins-themes-sync.php' ) ?>
-				<br>
-				<?php fs_require_template( '/debug/logger.php' ) ?>
+		return number_format( 100 * $total_time, 2 ) . ' ' . fs_text_x_inline( 'ms', 'milliseconds' );
+	}
+
+	public function render() {
+		?>
+		<div id='debug-bar-php'>
+			<?php fs_require_template( '/debug/api-calls.php' ); ?>
+			<br>
+			<?php fs_require_template( '/debug/scheduled-crons.php' ); ?>
+			<br>
+			<?php fs_require_template( '/debug/plugins-themes-sync.php' ); ?>
+			<br>
+			<?php fs_require_template( '/debug/logger.php' ); ?>
 			</div>
 		<?php
-		}
 	}
+}
