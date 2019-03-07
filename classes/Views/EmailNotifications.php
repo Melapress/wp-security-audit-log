cd8cd0cac4c71b1a35170f02b22a8eef97034dec
Ashar Irfan <mrasharirfan@gmail.com>
2019-03-07 14:46:39 +0500

    👌 IMPROVE: Email Notifications

--- classes/Views/EmailNotifications.php
+++ classes/Views/EmailNotifications.php
@@ -39,7 +39,7 @@ class WSAL_Views_EmailNotifications extends WSAL_AbstractView {
 	 * Method: Get View Name.
 	 */
 	public function GetName() {
-		return __( 'Email Notifications &#8682;', 'wp-security-audit-log' );
+		return __( 'Notifications &#8682;', 'wp-security-audit-log' );
 	}
 
 	/**
@@ -82,7 +82,8 @@ class WSAL_Views_EmailNotifications extends WSAL_AbstractView {
 			'wsal-swipebox-js',
 			$this->_plugin->GetBaseUrl() . '/js/jquery.swipebox.min.js',
 			array( 'jquery' ),
-			filemtime( $this->_plugin->GetBaseDir() . '/js/jquery.swipebox.min.js' )
+			filemtime( $this->_plugin->GetBaseDir() . '/js/jquery.swipebox.min.js' ),
+			false
 		);
 		wp_enqueue_script( 'wsal-swipebox-js' );
 
@@ -91,7 +92,8 @@ class WSAL_Views_EmailNotifications extends WSAL_AbstractView {
 			'wsal-extensions-js',
 			$this->_plugin->GetBaseUrl() . '/js/extensions.js',
 			array( 'wsal-swipebox-js' ),
-			filemtime( $this->_plugin->GetBaseDir() . '/js/extensions.js' )
+			filemtime( $this->_plugin->GetBaseDir() . '/js/extensions.js' ),
+			false
 		);
 		wp_enqueue_script( 'wsal-extensions-js' );
 	}
@@ -124,34 +126,35 @@ class WSAL_Views_EmailNotifications extends WSAL_AbstractView {
 						</ul>
 					</p>
 					<?php
+					// Trial link arguments.
+					$trial_args = array(
+						'page'          => 'wsal-auditlog-pricing',
+						'billing_cycle' => 'annual',
+						'trial'         => 'true',
+					);
+
 					// Buy Now button link.
-					$buy_now = add_query_arg( 'page', 'wsal-auditlog-pricing', admin_url( 'admin.php' ) );
+					$buy_now        = add_query_arg( 'page', 'wsal-auditlog-pricing', admin_url( 'admin.php' ) );
 					$buy_now_target = '';
+					$trial_link     = add_query_arg( $trial_args, admin_url( 'admin.php' ) );
 
 					// If user is not super admin and website is multisite then change the URL.
 					if ( $this->_plugin->IsMultisite() && ! is_super_admin() ) {
-						$buy_now = 'https://www.wpsecurityauditlog.com/pricing/';
+						$buy_now        = 'https://www.wpsecurityauditlog.com/pricing/';
+						$trial_link     = 'https://www.wpsecurityauditlog.com/pricing/';
 						$buy_now_target = 'target="_blank"';
 					} elseif ( $this->_plugin->IsMultisite() && is_super_admin() ) {
-						$buy_now = add_query_arg( 'page', 'wsal-auditlog-pricing', network_admin_url( 'admin.php' ) );
+						$buy_now    = add_query_arg( 'page', 'wsal-auditlog-pricing', network_admin_url( 'admin.php' ) );
+						$trial_link = add_query_arg( $trial_args, network_admin_url( 'admin.php' ) );
 					} elseif ( ! $this->_plugin->IsMultisite() && ! current_user_can( 'manage_options' ) ) {
-						$buy_now = 'https://www.wpsecurityauditlog.com/pricing/';
+						$buy_now        = 'https://www.wpsecurityauditlog.com/pricing/';
+						$trial_link     = 'https://www.wpsecurityauditlog.com/pricing/';
 						$buy_now_target = 'target="_blank"';
 					}
-
-					$more_info = add_query_arg(
-						array(
-							'utm_source' => 'plugin',
-							'utm_medium' => 'page',
-							'utm_content' => 'email+more+info',
-							'utm_campaign' => 'upgrade+premium',
-						),
-						'https://www.wpsecurityauditlog.com/premium-features/'
-					);
 					?>
 					<p>
 						<a class="button-primary wsal-extension-btn" href="<?php echo esc_attr( $buy_now ); ?>" <?php echo esc_attr( $buy_now_target ); ?>><?php esc_html_e( 'Upgrade to Premium', 'wp-security-audit-log' ); ?></a>
-						<a class="button-primary wsal-extension-btn" href="<?php echo esc_attr( $more_info ); ?>" target="_blank"><?php esc_html_e( 'More Information', 'wp-security-audit-log' ); ?></a>
+						<a class="button-primary wsal-extension-btn" href="<?php echo esc_attr( $trial_link ); ?>" <?php echo esc_attr( $buy_now_target ); ?>><?php esc_html_e( 'Start Free Trial', 'wp-security-audit-log' ); ?></a>
 					</p>
 				</div>
 				<!-- /.wsal-col -->
@@ -160,8 +163,25 @@ class WSAL_Views_EmailNotifications extends WSAL_AbstractView {
 
 			<div class="wsal-row">
 				<div class="wsal-col">
+					<p>
+						<?php
+						$more_info = add_query_arg(
+							array(
+								'utm_source'   => 'plugin',
+								'utm_medium'   => 'page',
+								'utm_content'  => 'users+sessions+more+info',
+								'utm_campaign' => 'upgrade+premium',
+							),
+							'https://www.wpsecurityauditlog.com/premium-features/'
+						);
+						echo sprintf(
+							/* Translators: Learn more hyperlink */
+							esc_html__( '%s about all the other premium features and how you can use them to better manage your WordPress site and users.', 'wp-security-audit-log' ),
+							'<a href="' . esc_url( $more_info ) . '" target="_blank">' . esc_html__( 'Learn more', 'wp-security-audit-log' ) . '</a>'
+						);
+						?>
+					</p>
 					<h3><?php esc_html_e( 'Screenshots', 'wp-security-audit-log' ); ?></h3>
-
 					<p>
 						<ul class="wsal-features-list">
 							<li>
@@ -180,10 +200,9 @@ class WSAL_Views_EmailNotifications extends WSAL_AbstractView {
 							</li>
 						</ul>
 					</p>
-
 					<p>
 						<a class="button-primary wsal-extension-btn" href="<?php echo esc_attr( $buy_now ); ?>" <?php echo esc_attr( $buy_now_target ); ?>><?php esc_html_e( 'Upgrade to Premium', 'wp-security-audit-log' ); ?></a>
-						<a class="button-primary wsal-extension-btn" href="<?php echo esc_attr( $more_info ); ?>" target="_blank"><?php esc_html_e( 'More Information', 'wp-security-audit-log' ); ?></a>
+						<a class="button-primary wsal-extension-btn" href="<?php echo esc_attr( $trial_link ); ?>" <?php echo esc_attr( $buy_now_target ); ?>><?php esc_html_e( 'Start Free Trial', 'wp-security-audit-log' ); ?></a>
 					</p>
 				</div>
 			</div>
