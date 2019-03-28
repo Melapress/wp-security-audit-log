1874d283cfc2d777957bc217e3605e46a7237f0d
Ashar Irfan <mrasharirfan@gmail.com>
2019-03-28 16:50:22 +0500

    👌 IMPROVE: External DB Addon View

--- classes/Views/ExternalDB.php
+++ classes/Views/ExternalDB.php
@@ -99,114 +99,34 @@ class WSAL_Views_ExternalDB extends WSAL_AbstractView {
 	}
 
 	/**
-	 * Method: Get View.
+	 * Page View.
 	 */
 	public function Render() {
-		?>
-		<div class="wrap-advertising-page-single">
-			<div class="wsal-row">
-				<div class="wsal-col">
-					<div class="icon" style='background-image:url("<?php echo esc_url( $this->_plugin->GetBaseUrl() ); ?>/img/database.jpg");'></div>
-				</div>
-				<!-- /.wsal-col -->
+		$title        = __( 'Audit Log Database & Integration Tools', 'wp-security-audit-log' );
+		$description  = __( 'There are several benefits to segregating the logs from the main site database, and to be able to mirror the logs to third party and centralized business solutions. Upgrade to premium and:', 'wp-security-audit-log' );
+		$addon_img    = trailingslashit( WSAL_BASE_URL ) . 'img/' . $this->GetSafeViewName() . '.jpg';
+		$premium_list = array(
+			__( 'Store the audit logs of your sites on an external database', 'wp-security-audit-log' ),
+			__( 'Configuring archiving and store older log data in a segregated database', 'wp-security-audit-log' ),
+			__( 'Mirror the logs to syslog, Slack, Papertrail and central business communication services', 'wp-security-audit-log' ),
+			__( 'Configure filters to filter what is mirrored and archived in the databases and services', 'wp-security-audit-log' ),
+		);
+		$subtext      = false;
+		$screenshots  = array(
+			array(
+				'desc' => __( 'Easily configure integration and database connections thanks to a user friendly wizard.', 'wp-security-audit-log' ),
+				'img'  => trailingslashit( WSAL_BASE_URL ) . 'img/external-db/db_integrations_1.png',
+			),
+			array(
+				'desc' => __( 'Configure activity log filters for third party services connections.', 'wp-security-audit-log' ),
+				'img'  => trailingslashit( WSAL_BASE_URL ) . 'img/external-db/db_integrations_2.png',
+			),
+			array(
+				'desc' => __( 'Configure an unlimited number of connections to different databases and third party services.', 'wp-security-audit-log' ),
+				'img'  => trailingslashit( WSAL_BASE_URL ) . 'img/external-db/db_integrations_3.png',
+			),
+		);
 
-				<div class="wsal-col">
-					<h3><?php esc_html_e( 'External DB', 'wp-security-audit-log' ); ?></h3>
-					<p>
-						<?php esc_html_e( 'Upgrade to Premium to:', 'wp-security-audit-log' ); ?>
-					</p>
-					<p>
-						<ul class="wsal-features-list">
-							<li><?php esc_html_e( 'Move the audit log to an external database for improved security & performance,', 'wp-security-audit-log' ); ?></li>
-							<li><?php esc_html_e( 'Centralize the audit log in your centralized logging system,', 'wp-security-audit-log' ); ?></li>
-							<li><?php esc_html_e( 'Mirror the audit trail to Syslog, Papertrail etc,', 'wp-security-audit-log' ); ?></li>
-							<li><?php esc_html_e( 'Configure archiving rules to archive old events in an archiving database,', 'wp-security-audit-log' ); ?></li>
-							<li><?php esc_html_e( '& more.', 'wp-security-audit-log' ); ?></li>
-						</ul>
-					</p>
-					<?php
-					// Trial link arguments.
-					$trial_args = array(
-						'page'          => 'wsal-auditlog-pricing',
-						'billing_cycle' => 'annual',
-						'trial'         => 'true',
-					);
-
-					// Buy Now button link.
-					$buy_now        = add_query_arg( 'page', 'wsal-auditlog-pricing', admin_url( 'admin.php' ) );
-					$buy_now_target = '';
-					$trial_link     = add_query_arg( $trial_args, admin_url( 'admin.php' ) );
-
-					// If user is not super admin and website is multisite then change the URL.
-					if ( $this->_plugin->IsMultisite() && ! is_super_admin() ) {
-						$buy_now        = 'https://www.wpsecurityauditlog.com/pricing/';
-						$trial_link     = 'https://www.wpsecurityauditlog.com/pricing/';
-						$buy_now_target = 'target="_blank"';
-					} elseif ( $this->_plugin->IsMultisite() && is_super_admin() ) {
-						$buy_now    = add_query_arg( 'page', 'wsal-auditlog-pricing', network_admin_url( 'admin.php' ) );
-						$trial_link = add_query_arg( $trial_args, network_admin_url( 'admin.php' ) );
-					} elseif ( ! $this->_plugin->IsMultisite() && ! current_user_can( 'manage_options' ) ) {
-						$buy_now        = 'https://www.wpsecurityauditlog.com/pricing/';
-						$trial_link     = 'https://www.wpsecurityauditlog.com/pricing/';
-						$buy_now_target = 'target="_blank"';
-					}
-					?>
-					<p>
-						<a class="button-primary wsal-extension-btn" href="<?php echo esc_attr( $buy_now ); ?>" <?php echo esc_attr( $buy_now_target ); ?>><?php esc_html_e( 'Upgrade to Premium', 'wp-security-audit-log' ); ?></a>
-						<a class="button-primary wsal-extension-btn" href="<?php echo esc_attr( $trial_link ); ?>" <?php echo esc_attr( $buy_now_target ); ?>><?php esc_html_e( 'Start Free Trial', 'wp-security-audit-log' ); ?></a>
-					</p>
-				</div>
-				<!-- /.wsal-col -->
-			</div>
-			<!-- /.wsal-row -->
-
-			<div class="wsal-row">
-				<div class="wsal-col">
-					<p>
-						<?php
-						$more_info = add_query_arg(
-							array(
-								'utm_source'   => 'plugin',
-								'utm_medium'   => 'page',
-								'utm_content'  => 'users+sessions+more+info',
-								'utm_campaign' => 'upgrade+premium',
-							),
-							'https://www.wpsecurityauditlog.com/premium-features/'
-						);
-						echo sprintf(
-							/* Translators: Learn more hyperlink */
-							esc_html__( '%s about all the other premium features and how you can use them to better manage your WordPress site and users.', 'wp-security-audit-log' ),
-							'<a href="' . esc_url( $more_info ) . '" target="_blank">' . esc_html__( 'Learn more', 'wp-security-audit-log' ) . '</a>'
-						);
-						?>
-					</p>
-					<h3><?php esc_html_e( 'Screenshots', 'wp-security-audit-log' ); ?></h3>
-					<p>
-						<ul class="wsal-features-list">
-							<li>
-								<?php esc_html_e( 'Configure an external database so the WordPress audit trail is stored on it instead of the WordPress database.', 'wp-security-audit-log' ); ?><br />
-								<a class="swipebox" title="<?php esc_attr_e( 'Configure an external database so the WordPress audit trail is stored on it instead of the WordPress database.', 'wp-security-audit-log' ); ?>"
-									href="<?php echo esc_url( $this->_plugin->GetBaseUrl() ); ?>/img/external-db/external_database_1.png">
-									<img width="500" src="<?php echo esc_url( $this->_plugin->GetBaseUrl() ); ?>/img/external-db/external_database_1.png">
-								</a>
-							</li>
-							<li>
-								<?php esc_html_e( 'Configure mirroring to keep a secondary copy of the WordPress audit trail on Syslog, Papertrail etc.', 'wp-security-audit-log' ); ?><br />
-								<a class="swipebox" title="<?php esc_attr_e( 'Configure mirroring to keep a secondary copy of the WordPress audit trail on Syslog, Papertrail etc.', 'wp-security-audit-log' ); ?>"
-									href="<?php echo esc_url( $this->_plugin->GetBaseUrl() ); ?>/img/external-db/extermal_database_mirroring_options.png">
-									<img width="500" src="<?php echo esc_url( $this->_plugin->GetBaseUrl() ); ?>/img/external-db/extermal_database_mirroring_options.png">
-								</a>
-							</li>
-						</ul>
-					</p>
-					<p>
-						<a class="button-primary wsal-extension-btn" href="<?php echo esc_attr( $buy_now ); ?>" <?php echo esc_attr( $buy_now_target ); ?>><?php esc_html_e( 'Upgrade to Premium', 'wp-security-audit-log' ); ?></a>
-						<a class="button-primary wsal-extension-btn" href="<?php echo esc_attr( $trial_link ); ?>" <?php echo esc_attr( $buy_now_target ); ?>><?php esc_html_e( 'Start Free Trial', 'wp-security-audit-log' ); ?></a>
-					</p>
-				</div>
-			</div>
-			<!-- /.wsal-row -->
-		</div>
-		<?php
+		require_once dirname( __FILE__ ) . '/addons/html-view.php';
 	}
 }
