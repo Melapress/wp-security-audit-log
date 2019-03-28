28c6a9fc63bf53bbe8cb06c0e77f468db9850432
Ashar Irfan <mrasharirfan@gmail.com>
2019-03-28 16:50:15 +0500

    👌 IMPROVE: Notifications Addon View

--- classes/Views/EmailNotifications.php
+++ classes/Views/EmailNotifications.php
@@ -99,115 +99,40 @@ class WSAL_Views_EmailNotifications extends WSAL_AbstractView {
 	}
 
 	/**
-	 * Method: Get View Render.
+	 * Page View.
 	 */
 	public function Render() {
-		?>
-		<div class="wrap-advertising-page-single">
-			<div class="wsal-row">
-				<div class="wsal-col">
-					<div class="icon" style='background-image:url("<?php echo esc_url( $this->_plugin->GetBaseUrl() ); ?>/img/envelope.jpg");'></div>
-				</div>
-				<!-- /.wsal-col -->
+		$title        = __( 'SMS & Email Notifications', 'wp-security-audit-log' );
+		$description  = __( 'Get instantly alerted of important changes on your site via SMS and email notifications. Upgrade to premium and:', 'wp-security-audit-log' );
+		$addon_img    = trailingslashit( WSAL_BASE_URL ) . 'img/' . $this->GetSafeViewName() . '.jpg';
+		$premium_list = array(
+			__( 'Configure any type of SMS & email notifications', 'wp-security-audit-log' ),
+			__( 'Receive notifications for when users login, change their password or change content', 'wp-security-audit-log' ),
+			__( 'Get alerted of site changes like plugin installs, theme changes etc', 'wp-security-audit-log' ),
+			__( 'Enable built-in security email notifications of suspicious user activity', 'wp-security-audit-log' ),
+			__( 'Personalize all email and SMS templates', 'wp-security-audit-log' ),
+			__( 'Use the trigger builder to configure any type of notification criteria!', 'wp-security-audit-log' ),
+		);
+		$subtext      = __( 'Getting started is really easy. You can use one of the plugin’s built-in notifications or create your own using the easy to use trigger builder.', 'wp-security-audit-log' );
+		$screenshots  = array(
+			array(
+				'desc' => __( 'Email and SMS notifications instantly alert you of important changes on your WordPress site.', 'wp-security-audit-log' ),
+				'img'  => trailingslashit( WSAL_BASE_URL ) . 'img/notifications/notifications_1.png',
+			),
+			array(
+				'desc' => __( 'Easily enable any of the built-in security and user management notifications.', 'wp-security-audit-log' ),
+				'img'  => trailingslashit( WSAL_BASE_URL ) . 'img/notifications/notifications_2.png',
+			),
+			array(
+				'desc' => __( 'Use the trigger builder to configure any type of email and SMS notification to get instantly alerted of site changes that are important to you and your business.', 'wp-security-audit-log' ),
+				'img'  => trailingslashit( WSAL_BASE_URL ) . 'img/notifications/notifications_3.png',
+			),
+			array(
+				'desc' => __( 'All email and SMS templates are configurable, allowing you to personalize them.', 'wp-security-audit-log' ),
+				'img'  => trailingslashit( WSAL_BASE_URL ) . 'img/notifications/notifications_4.png',
+			),
+		);
 
-				<div class="wsal-col">
-					<h3><?php esc_html_e( 'Notifications', 'wp-security-audit-log' ); ?></h3>
-					<p>
-						<?php esc_html_e( 'Upgrade to Premium to:', 'wp-security-audit-log' ); ?>
-					</p>
-					<p>
-						<ul class="wsal-features-list">
-							<li><?php esc_html_e( 'Configure email notifications to be instantly alerted of important changes,', 'wp-security-audit-log' ); ?></li>
-							<li><?php esc_html_e( 'Configure notifications for when users login, change content, install a plugin or do any other change,', 'wp-security-audit-log' ); ?></li>
-							<li><?php esc_html_e( 'Configure security email notifications,', 'wp-security-audit-log' ); ?></li>
-							<li><?php esc_html_e( 'Configure email notifications via a user friendly wizard,', 'wp-security-audit-log' ); ?></li>
-							<li><?php esc_html_e( 'Edit and create your own templates for email notifications,', 'wp-security-audit-log' ); ?></li>
-							<li><?php esc_html_e( '& more.', 'wp-security-audit-log' ); ?></li>
-						</ul>
-					</p>
-					<?php
-					// Trial link arguments.
-					$trial_args = array(
-						'page'          => 'wsal-auditlog-pricing',
-						'billing_cycle' => 'annual',
-						'trial'         => 'true',
-					);
-
-					// Buy Now button link.
-					$buy_now        = add_query_arg( 'page', 'wsal-auditlog-pricing', admin_url( 'admin.php' ) );
-					$buy_now_target = '';
-					$trial_link     = add_query_arg( $trial_args, admin_url( 'admin.php' ) );
-
-					// If user is not super admin and website is multisite then change the URL.
-					if ( $this->_plugin->IsMultisite() && ! is_super_admin() ) {
-						$buy_now        = 'https://www.wpsecurityauditlog.com/pricing/';
-						$trial_link     = 'https://www.wpsecurityauditlog.com/pricing/';
-						$buy_now_target = 'target="_blank"';
-					} elseif ( $this->_plugin->IsMultisite() && is_super_admin() ) {
-						$buy_now    = add_query_arg( 'page', 'wsal-auditlog-pricing', network_admin_url( 'admin.php' ) );
-						$trial_link = add_query_arg( $trial_args, network_admin_url( 'admin.php' ) );
-					} elseif ( ! $this->_plugin->IsMultisite() && ! current_user_can( 'manage_options' ) ) {
-						$buy_now        = 'https://www.wpsecurityauditlog.com/pricing/';
-						$trial_link     = 'https://www.wpsecurityauditlog.com/pricing/';
-						$buy_now_target = 'target="_blank"';
-					}
-					?>
-					<p>
-						<a class="button-primary wsal-extension-btn" href="<?php echo esc_attr( $buy_now ); ?>" <?php echo esc_attr( $buy_now_target ); ?>><?php esc_html_e( 'Upgrade to Premium', 'wp-security-audit-log' ); ?></a>
-						<a class="button-primary wsal-extension-btn" href="<?php echo esc_attr( $trial_link ); ?>" <?php echo esc_attr( $buy_now_target ); ?>><?php esc_html_e( 'Start Free Trial', 'wp-security-audit-log' ); ?></a>
-					</p>
-				</div>
-				<!-- /.wsal-col -->
-			</div>
-			<!-- /.wsal-row -->
-
-			<div class="wsal-row">
-				<div class="wsal-col">
-					<p>
-						<?php
-						$more_info = add_query_arg(
-							array(
-								'utm_source'   => 'plugin',
-								'utm_medium'   => 'page',
-								'utm_content'  => 'users+sessions+more+info',
-								'utm_campaign' => 'upgrade+premium',
-							),
-							'https://www.wpsecurityauditlog.com/premium-features/'
-						);
-						echo sprintf(
-							/* Translators: Learn more hyperlink */
-							esc_html__( '%s about all the other premium features and how you can use them to better manage your WordPress site and users.', 'wp-security-audit-log' ),
-							'<a href="' . esc_url( $more_info ) . '" target="_blank">' . esc_html__( 'Learn more', 'wp-security-audit-log' ) . '</a>'
-						);
-						?>
-					</p>
-					<h3><?php esc_html_e( 'Screenshots', 'wp-security-audit-log' ); ?></h3>
-					<p>
-						<ul class="wsal-features-list">
-							<li>
-								<?php esc_html_e( 'Use the trigger builder to configure any type of email notification so you are instantly alerted of important changes on your WordPress.', 'wp-security-audit-log' ); ?><br />
-								<a class="swipebox" title="<?php esc_attr_e( 'Use the trigger builder to configure any type of email notification so you are instantly alerted of important changes on your WordPress.', 'wp-security-audit-log' ); ?>"
-									href="<?php echo esc_url( $this->_plugin->GetBaseUrl() ); ?>/img/email-notifications/email_notifications.png">
-									<img width="500" src="<?php echo esc_url( $this->_plugin->GetBaseUrl() ); ?>/img/email-notifications/email_notifications.png">
-								</a>
-							</li>
-							<li>
-								<?php esc_html_e( 'Use the wizard to easily get started and quickly configure basic email notifications.', 'wp-security-audit-log' ); ?><br />
-								<a class="swipebox" title="<?php esc_attr_e( 'Use the wizard to easily get started and quickly configure basic email notifications.', 'wp-security-audit-log' ); ?>"
-									href="<?php echo esc_url( $this->_plugin->GetBaseUrl() ); ?>/img/email-notifications/email_notifications_wizard.png">
-									<img width="500" src="<?php echo esc_url( $this->_plugin->GetBaseUrl() ); ?>/img/email-notifications/email_notifications_wizard.png">
-								</a>
-							</li>
-						</ul>
-					</p>
-					<p>
-						<a class="button-primary wsal-extension-btn" href="<?php echo esc_attr( $buy_now ); ?>" <?php echo esc_attr( $buy_now_target ); ?>><?php esc_html_e( 'Upgrade to Premium', 'wp-security-audit-log' ); ?></a>
-						<a class="button-primary wsal-extension-btn" href="<?php echo esc_attr( $trial_link ); ?>" <?php echo esc_attr( $buy_now_target ); ?>><?php esc_html_e( 'Start Free Trial', 'wp-security-audit-log' ); ?></a>
-					</p>
-				</div>
-			</div>
-			<!-- /.wsal-row -->
-		</div>
-		<?php
+		require_once dirname( __FILE__ ) . '/addons/html-view.php';
 	}
 }
