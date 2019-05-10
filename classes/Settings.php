706992491f615367dbb89ae85a1cbe43d801e091
Ashar Irfan <mrasharirfan@gmail.com>
2019-05-10 17:01:28 +0500

    📦 NEW: IPv4 range functions

--- classes/Settings.php
+++ classes/Settings.php
@@ -2005,4 +2005,99 @@ class WSAL_Settings {
 		return $wpdb->get_results( "SELECT * FROM $options_table" ); // phpcs:ignore
 	}
 
+	/**
+	 * Check if IP is in range for IPv4.
+	 *
+	 * This function takes 2 arguments, an IP address and a "range" in several different formats.
+	 *
+	 * Network ranges can be specified as:
+	 * 1. Wildcard format:     1.2.3.*
+	 * 2. CIDR format:         1.2.3/24  OR  1.2.3.4/255.255.255.0
+	 * 3. Start-End IP format: 1.2.3.0-1.2.3.255
+	 *
+	 * The function will return true if the supplied IP is within the range.
+	 * Note little validation is done on the range inputs - it expects you to
+	 * use one of the above 3 formats.
+	 *
+	 * @link https://github.com/cloudflarearchive/Cloudflare-Tools/blob/master/cloudflare/ip_in_range.php#L55
+	 *
+	 * @param string $ip    - IP address.
+	 * @param string $range - Range of IP address.
+	 * @return boolean
+	 */
+	public function check_ipv4_in_range( $ip, $range ) {
+		if ( strpos( $range, '/' ) !== false ) {
+			// $range is in IP/NETMASK format.
+			list($range, $netmask) = explode( '/', $range, 2 );
+
+			if ( strpos( $netmask, '.' ) !== false ) {
+				// $netmask is a 255.255.0.0 format.
+				$netmask     = str_replace( '*', '0', $netmask );
+				$netmask_dec = ip2long( $netmask );
+				return ( ( ip2long( $ip ) & $netmask_dec ) === ( ip2long( $range ) & $netmask_dec ) );
+			} else {
+				// $netmask is a CIDR size block
+				// fix the range argument.
+				$x       = explode( '.', $range );
+				$x_count = count( $x );
+
+				while ( $x_count < 4 ) {
+					$x[]     = '0';
+					$x_count = count( $x );
+				}
+
+				list($a,$b,$c,$d) = $x;
+				$range            = sprintf( '%u.%u.%u.%u', empty( $a ) ? '0' : $a, empty( $b ) ? '0' : $b, empty( $c ) ? '0' : $c, empty( $d ) ? '0' : $d );
+				$range_dec        = ip2long( $range );
+				$ip_dec           = ip2long( $ip );
+
+				// Strategy 1 - Create the netmask with 'netmask' 1s and then fill it to 32 with 0s
+				// $netmask_dec = bindec(str_pad('', $netmask, '1') . str_pad('', 32-$netmask, '0'));
+				// Strategy 2 - Use math to create it.
+				$wildcard_dec = pow( 2, ( 32 - $netmask ) ) - 1;
+				$netmask_dec  = ~ $wildcard_dec;
+
+				return ( ( $ip_dec & $netmask_dec ) === ( $range_dec & $netmask_dec ) );
+			}
+		} else {
+			// Range might be 255.255.*.* or 1.2.3.0-1.2.3.255.
+			if ( strpos( $range, '*' ) !== false ) { // a.b.*.* format
+				// Just convert to A-B format by setting * to 0 for A and 255 for B.
+				$lower = str_replace( '*', '0', $range );
+				$upper = str_replace( '*', '255', $range );
+				$range = "$lower-$upper";
+			}
+
+			// A-B format.
+			if ( strpos( $range, '-' ) !== false ) {
+				list($lower, $upper) = explode( '-', $range, 2 );
+				$lower_dec           = (float) sprintf( '%u', ip2long( $lower ) );
+				$upper_dec           = (float) sprintf( '%u', ip2long( $upper ) );
+				$ip_dec              = (float) sprintf( '%u', ip2long( $ip ) );
+				return ( ( $ip_dec >= $lower_dec ) && ( $ip_dec <= $upper_dec ) );
+			}
+
+			return false;
+		}
+	}
+
+	/**
+	 * Return the range of IP address from 127.0.0.0-24 to 127.0.0.0-127.0.0.24 format.
+	 *
+	 * @param string $range - Range of IP address.
+	 * @return object
+	 */
+	public function get_ipv4_by_range( $range ) {
+		list($lower_ip, $upper_ip) = explode( '-', $range, 2 );
+
+		$lower_arr = explode( '.', $lower_ip );
+		$count     = count( $lower_arr );
+		unset( $lower_arr[ $count - 1 ] );
+		$upper_ip = implode( '.', $lower_arr ) . '.' . $upper_ip;
+
+		return (object) array(
+			'lower' => $lower_ip,
+			'upper' => $upper_ip,
+		);
+	}
 }
